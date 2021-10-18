59b81fd2e67c54d523e94bff4aaede6273d02d69
jailtonmendes <jailtonmendes.silva@gmail.com>
2021-10-18 18:03:56 -0300

    Subindo projeto

---
+++ src/app/courses/courses/courses.component.ts
@@ -0,0 +1,27 @@
+import { CoursesService } from './../services/courses.service';
+import { Course } from './../model/course';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss']
+})
+export class CoursesComponent implements OnInit {
+
+  courses: Observable<Course[]>;
+  displayedColumns = ['name','category'];
+
+
+  constructor(private CoursesService: CoursesService) {
+
+    this.courses = this.CoursesService.list();
+
+   }
+
+  ngOnInit(): void {
+
+  }
+
+}
